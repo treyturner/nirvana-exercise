0e23c37f7a0005dae2ac30e44ec29a58e69d4659
Glenna Yu <glenna@meetnirvana.com>
2022-08-17 12:15:08 -0400

    test: cover more edge cases

--- gilded_rose/test_gilded_rose.py
+++ gilded_rose/test_gilded_rose.py
@@ -14,41 +14,44 @@ def assert_same_items(item1: Item, item2: Item):
 def test_gilded_rose(days=1):
     items = [
              Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
+             Item(name="Aged Brie", sell_in=1, quality=0),
              Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             Item(name="Elixir of the Mongoose", sell_in=1, quality=5),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=30),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
              Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
              Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
              Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+             # Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
     gilded_rose = GildedRose(items)
     updated_items = [
         [
             Item(name="+5 Dexterity Vest", sell_in=9, quality=19),
-            Item(name="Aged Brie", sell_in=1, quality=1),
+            Item(name="Aged Brie", sell_in=0, quality=1),
             Item(name="Elixir of the Mongoose", sell_in=4, quality=6),
+            Item(name="Elixir of the Mongoose", sell_in=0, quality=4),
             Item(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=30),
             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=14, quality=21),
             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=9, quality=50),
             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=4, quality=50),
-            Item(name="Conjured Mana Cake", sell_in=2, quality=4),  # <-- :O
+            # Item(name="Conjured Mana Cake", sell_in=2, quality=4),  # <-- :O
         ],
         [
             Item(name="+5 Dexterity Vest", sell_in=8, quality=18),
-            Item(name="Aged Brie", sell_in=0, quality=2),
+            Item(name="Aged Brie", sell_in=-1, quality=3),
             Item(name="Elixir of the Mongoose", sell_in=3, quality=5),
+            Item(name="Elixir of the Mongoose", sell_in=-1, quality=2),
             Item(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=30),
             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=13, quality=22),
             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=8, quality=50),
             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=3, quality=50),
-            Item(name="Conjured Mana Cake", sell_in=1, quality=2),  # <-- :O
+            # Item(name="Conjured Mana Cake", sell_in=1, quality=2),  # <-- :O
         ],
     ]
     assert days <= len(updated_items), "TODO: handle more days"
